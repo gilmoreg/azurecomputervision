99cdf228ae9b1c5c0f7610654c24f7cbf8e965c0
Grayson Gilmore <grgilmor@microsoft.com>
2021-05-28 20:50:54 -0700

    POC

---
+++ AzureComputerVision/DocumentProcessor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using Newtonsoft.Json;
+
+namespace AzureComputerVision
+{
+    internal class DocumentProcessor
+    {
+        private readonly IComputerVisionClient _computerVisionClient;
+
+        public DocumentProcessor(IComputerVisionClient computerVisionClient)
+        {
+            _computerVisionClient = computerVisionClient;
+        }
+
+        public async ValueTask<IEnumerable<string>> ProcessDocument(string path)
+        {
+            // Read text from URL
+            var textHeaders = await _computerVisionClient.ReadInStreamAsync(File.OpenRead(path), language: "ja");
+            // After the request, get the operation location (operation ID)
+            string operationLocation = textHeaders.OperationLocation;
+
+            // Retrieve the URI where the recognized text will be stored from the Operation-Location header.
+            // We only need the ID and not the full URL
+            const int numberOfCharsInOperationId = 36;
+            string operationId = operationLocation.Substring(operationLocation.Length - numberOfCharsInOperationId);
+
+            // Extract the text
+            ReadOperationResult results;
+            do
+            {
+                results = await _computerVisionClient.GetReadResultAsync(Guid.Parse(operationId));
+            }
+            while ((results.Status == OperationStatusCodes.Running ||
+                results.Status == OperationStatusCodes.NotStarted));
+
+            File.WriteAllText("output.json", JsonConvert.SerializeObject(results.AnalyzeResult.ReadResults, Formatting.Indented));
+            
+
+            return results.AnalyzeResult.ReadResults.SelectMany(r => r.Lines.Select(l => l.Text).Reverse());
+        }
+
+        public async ValueTask<IEnumerable<string>> ProcessDocuments(IEnumerable<string> paths)
+        {
+            return await ProcessDocument(paths.First());
+            
+            // var tasks = paths.Select(p => ProcessDocument(p).AsTask());
+            //var result = await Task.WhenAll(tasks);
+            //return result.SelectMany(r => r);
+        }
+    }
+}
---
+++ AzureComputerVision/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+
+namespace AzureComputerVision
+{
+    internal static class Program
+    {
+        private const string subscriptionKeyArg = "/subscriptionKey=";
+        private const string endpointArg = "/endpoint=";
+        private const string folderArg = "/folder=";
+
+        // Example arguments.
+        // /subscriptionKey=xyz /endpoint=https://contoso.cognitiveservices.azure.com/ /directory=d:\pageImages
+        private static async Task Main(string[] args)
+        {
+            string subscriptionKey = null;
+            string endpoint = null;
+            string folder = null;
+
+            foreach (string arg in args)
+            {
+                if (arg.StartsWith(subscriptionKeyArg, StringComparison.OrdinalIgnoreCase))
+                {
+                    subscriptionKey = arg.Remove(0, subscriptionKeyArg.Length);
+                }
+
+                if (arg.StartsWith(endpointArg, StringComparison.OrdinalIgnoreCase))
+                {
+                    endpoint = arg.Remove(0, endpointArg.Length);
+                }
+
+                if (arg.StartsWith(folderArg, StringComparison.OrdinalIgnoreCase))
+                {
+                    folder = arg.Remove(0, folderArg.Length);
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(subscriptionKey))
+            {
+                throw new ArgumentNullException(nameof(subscriptionKey));
+            }
+
+            if (string.IsNullOrWhiteSpace(endpoint))
+            {
+                throw new ArgumentNullException(nameof(endpoint));
+            }
+
+            if (!Directory.Exists(folder))
+            {
+                throw new ArgumentException("folder does not exist.");
+            }
+
+            var files = Directory.EnumerateFiles(folder, "*.*", SearchOption.AllDirectories);
+
+            if (!files.Any())
+            {
+                throw new ArgumentException($"Unable to find any files in {folder}");
+            }
+
+            // Create a client
+            var client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(subscriptionKey)) { Endpoint = endpoint };
+            var processor = new DocumentProcessor(client);
+
+            var results = await processor.ProcessDocuments(files);
+            await File.WriteAllLinesAsync("output.txt", results);
+        }
+    }
+}
